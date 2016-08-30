29beba7a81055265cf5eb2ba3a33fa6c462f6ebb
FaySie <sel12342001@gmail.com>
2016-08-30 15:23:24 +0800

    rocket animate

---
+++ js/style.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('.start').on('click',function() {
+		$(this).fadeOut();
+		
+		$('.bg').css('background-position','top right');
+		$('.bg').css('transition','2s');
+		
+		setTimeout(function() {
+			$('.rocket').css('transform','translate(-80%, -70%)');
+			$('.rocket').css('transition','0.5s');
+		},1000);
+
+		setTimeout(function() {
+			$('.rocket').css('transform','translate(250%, -70%)');
+			$('.rocket').css('transition','0.8s');
+			// $('.bg').fadeOut('slow');
+			// $('#content').fadeIn('slow');
+		},1500);
+	});
+});
